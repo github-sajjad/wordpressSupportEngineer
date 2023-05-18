6153485566e034ad283bb0c3cd4f4971d67b3a7d
Sajjad <123508448+github-sajjad@users.noreply.github.com>
2023-05-18 08:30:40 +0600

    Add files via upload

---
+++ Module 4 PHP/phpBacisPart-3 define function and basic.php
@@ -0,0 +1,7 @@
+<?php
+
+define('A',10);
+define('B',20);
+define('C',30);
+
+echo A+B+C;
---
+++ Module 4 PHP/phpBacisPart-4 expression and mathematical operator.php
@@ -0,0 +1,33 @@
+<?php
+
+$num1 = 10;
+$num2 = 6;
+
+echo 'Number1 = 10 ';
+echo '<br>';
+echo 'Number2 = 6 ';
+
+echo '<br>';
+echo '<br>';
+echo 'Summation: ';
+echo $num1 + $num2;
+echo '<br>';
+
+echo 'Subtraction: ';
+echo $num1 - $num2;
+echo '<br>';
+
+echo 'Multiplication: ';
+echo $num1 * $num2;
+echo '<br>';
+
+echo 'Divition: ';
+echo $num1 / $num2;
+echo '<br>';
+
+echo 'Modulus: ';
+echo $num1 % $num2;
+echo '<br>';
+
+echo 'Exponential:  ';
+echo $num1 ** $num2;
