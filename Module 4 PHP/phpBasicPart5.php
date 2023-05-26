b7fe04b4fd6f9100c0b375f5c99a2027758441fc
Sajjad <123508448+github-sajjad@users.noreply.github.com>
2023-05-26 11:16:37 +0600

    Add files via upload

---
+++ Module 4 PHP/phpBasicPart5.php
@@ -0,0 +1,15 @@
+<?php
+//assignment operator
+$name = 'Sajjad ';
+$name .= 'Hossain';
+
+echo $name;
+
+// comparison operator
+var_dump(10<5);
+var_dump(10<5);
+var_dump(10<=10);
+var_dump(10==10);
+var_dump(10!=5);
+var_dump('10'==10);
+var_dump('10'===10);
