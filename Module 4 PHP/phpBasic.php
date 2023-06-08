ad8db418ed1c9e67c0c73994817669554527d68f
Sajjad <123508448+github-sajjad@users.noreply.github.com>
2023-06-08 12:00:46 +0600

    Update phpBasic.php
    part 6, 7, 8, 9

--- Module 4 PHP/phpBasic.php
+++ Module 4 PHP/phpBasic.php
@@ -1,5 +1,6 @@
 <?php
 
+//Part 6
 $day='Saturday';
 
 if($day === 'Friday' || $day==='Saturday'){
@@ -8,3 +9,41 @@ if($day === 'Friday' || $day==='Saturday'){
 else{
     echo 'Today is not';
 }
+echo'</br>';
+
+//Part 7
+$array1=['Sajjad',200101013, 3.51];
+echo $array1[0];
+echo'</br>';
+
+//part 8
+$array2=[
+    'first-name' => 'Sajjad',
+    'last-name' => 'Hossain',
+    'nationality' => 'Bangladeshi',
+    'age' => 23
+];
+echo $array2['nationality'];
+echo'</br>';
+
+//part 9
+$array3=[
+    'first_name' => 'Sajjad',
+    'last_name' => 'Hossain',
+    'address' => [
+        'street_address'=>'House 9, road 3',
+        'country'=>'Bangladesh',
+        'city'=>'Dhaka',
+        'zipcode'=>'1200'
+    ],
+    'mobile_number'=>[
+        'country_prefix'=> '880',
+        'grameenphone'=>[
+            'gp1'=>'01700000000',
+            'gp2'=>'01712345678'
+        ]
+    ]
+];
+echo $array3['mobile_number']['grameenphone']['gp1'];
+
+//part 10
